439d29e04e9603a4e6e82a750368ed87fad75086
Wout Neirynck <wout@monkey-projects.be>
2024-02-21 21:05:39 +0100

    Display m2 dir

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -1,3 +1,14 @@
-(require '[monkey.ci.plugin.clj :as clj])
+(ns zeromq.build
+  (:require [monkey.ci.plugin.clj :as clj]
+            [monkey.ci.build
+             [core :as bc]
+             [shell :as s]]))
 
-(clj/deps-library {:test-alias :jeromq:test:junit})
+#_(clj/deps-library {:test-alias :jeromq:test:junit})
+
+(defn clj-test [ctx]
+  (-> (clj/clj-deps ctx "-X:jeromq:test:junit")
+      (update :script concat ["ls -l", (format "echo 'm2 dir: %s'" (s/in-work ctx ".m2"))])))
+
+(bd/defpipeline run-tests
+  [clj-test])
